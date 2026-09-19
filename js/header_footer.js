5f9f50e2a59ca2c8d4148e72190957db85756a41
hihiquan2010 <uyp079658@gmail.com>
2026-09-19 23:04:03 +0700

    Refactor header and footer rendering functions

--- js/header_footer.js
+++ js/header_footer.js
@@ -49,6 +49,19 @@ function renderFooter() {
     </span>
 
     <div class="infoGroup">
+      <!-- Histats.com  (div with counter) --><div id="histats_counter"></div>
+      <!-- Histats.com  START  (aync)-->
+      <script type="text/javascript">var _Hasync= _Hasync|| [];
+      _Hasync.push(['Histats.start', '1,5053294,4,383,112,48,00011110']);
+      _Hasync.push(['Histats.fasi', '1']);
+      _Hasync.push(['Histats.track_hits', '']);
+      (function() {
+      var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+      hs.src = ('//s10.histats.com/js15_as.js');
+      (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+      })();</script>
+      <noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?5053294&101" alt="" border="0"></a></noscript>
+      <!-- Histats.com  END  -->
       <b><p id="dv">
           <i class="fa-solid fa-school"></i> Đơn vị: Trường THPT Bình Chánh
         </p></b>
